9eb09797424c64707a5229d8d7d46d9d61ab645f
ANBUCHELVAN GANESAN <anbuchelvanganesan.cse2024@citchennai.edu.in>
2026-01-29 12:09:13 +0530

    Update index.ts

--- supabase/functions/analyze-media/index.ts
+++ supabase/functions/analyze-media/index.ts
@@ -5,7 +5,7 @@ const corsHeaders = {
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
 };
 
-interface HeatmapRegion {
+interface HeatmapRegion { 
   x: number;
   y: number;
   radius: number;
